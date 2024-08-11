eba3a5efd3984e77327c6bce8a6ea0d90916af4b
shahul01 <shahulhammeed01@gmail.com>
2024-08-11 13:55:24 +0530

    fix:be: changed hello() to ping()

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return server running message', () => {
+      const expectedText = 'NestJs server is running!';
+
+      expect(appController.ping()).toBe(expectedText);
     });
   });
 });
